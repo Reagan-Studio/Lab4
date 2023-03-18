d976accf615bd234e029973d6f1eceb26d49b0de
Regina <Regina@LAPTOP-39HSGRPC>
2023-03-18 14:15:14 +0300

    Добавьте файлы проекта.

---
+++ DymArray.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class DymArray
+    {
+        private double[] _arr = Array.Empty<double>();
+        private int _capacity;
+        public int Size { get; private set; }
+
+
+        public DymArray() { }
+        public DymArray(int capacity)
+        {
+            if (capacity < 0) throw new Exception("Задана некорректная мощность массива");
+
+            if (capacity == 0)
+            {
+                _capacity = 0;
+                Size = 0;
+            }
+            else
+            {
+                _arr = new double[capacity];
+                _capacity = capacity;
+            }
+        }
+        public DymArray(double[] arr)
+        {
+            Size = arr.Length;
+            _capacity = arr.Length;
+            AddSpace(arr.Length);
+            arr.CopyTo(_arr, 0);
+        }
+
+
+        public int Count => Size;
+
+
+        private double GetElement(int index)
+        {
+            if (index < 0 || index >= Size) throw new Exception("Задан некорректный индекс");
+            return _arr[index];
+        }
+        private void SetElement(double value, int index)
+        {
+            if (index < 0 || index >= Size) throw new Exception("Задан некорректный индекс");
+            _arr[index] = value;
+        }
+        public double this[int index]
+        {
+            get => GetElement(index);
+            set => SetElement(value, index);
+        }
+
+
+        private void AddSpace(int size = 1)
+        {
+            if (Size != 0)
+            {
+                _capacity = Convert.ToInt32(_capacity * 1.6 + 1);
+            }
+            else
+            {
+                if (size != 1)
+                {
+                    _capacity = Convert.ToInt32(size * 1.6 + 1);
+                }
+                else
+                {
+                    _capacity = size;
+                }
+            }
+            double[] tmp = new double[_capacity];
+            _arr.CopyTo(tmp, 0);
+            _arr = new double[tmp.Length];
+            _arr = tmp;
+        }
+
+
+        public void AppendToEnd(double value)
+        {
+            if (Size < _capacity)
+            {
+                _arr[Size++] = value;
+            }
+            else
+            {
+                AddSpace();
+                _arr[Size++] = value;
+            }
+        }
+
+
+        public void DeleteTail()
+        {
+            Size--;
+        }
+
+
+        public void Insert(double value, int index)
+        {
+            if (Size < _capacity)
+            {
+                Size++;
+            }
+            else
+            {
+                AddSpace();
+            }
+            for (int i = Size; i >= index; i--)
+            {
+                _arr[i] = _arr[i - 1];
+            }
+            _arr[index] = value;
+        }
+
+
+        public override string ToString()
+        {
+            return String.Join(" ", _arr[0..Size]);
+        }
+
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < Size; i++)
+            {
+                _arr[i] = _arr[i + 1];
+            }
+            Size--;
+        }
+
+
+        public void RemoveAll(double value)
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                if (_arr[i] != value)
+                {
+                    continue;
+                }
+                RemoveAt(i);
+            }
+        }
+
+
+        public int MaxIndex()
+        {
+            int index = 0;
+            for (int i = 0; i < Size; i++)
+            {
+                if (_arr[index] < _arr[i])
+                {
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Lab4;
+
+
+DymArray v1 = new DymArray();
+DymArray v2 = new DymArray(31);
+var arr = new double[] { 5, 7, 8, 9, 0, 3, 8, 9 };
+DymArray v3 = new DymArray(arr);
+
+//Console.WriteLine($"\nv1: " + v1);
+//Console.WriteLine($"\nv2: " + v2);
+//Console.WriteLine($"\nv3: " + v3);
+
+Console.WriteLine($"\nЧисло действительных элемeнтов в v3 = {v3.Count}");
+v3[0] = 56;
+Console.WriteLine("\nv3[0] = " + v3[0]);
+v2.AppendToEnd(67);
+v2.AppendToEnd(67);
+v2.AppendToEnd(67);
+v2.AppendToEnd(67);
+Console.WriteLine("\nv2: " + v2);
+v2.DeleteTail();
+Console.WriteLine("\nv2: " + v2);
+v3.Insert(10000, 4);
+Console.WriteLine("\nv3: " + v3);
+v3.RemoveAt(4);
+Console.WriteLine("\nv3: " + v3);
+v3.RemoveAll(8);
+Console.WriteLine("\nv3: " + v3);
+Console.WriteLine("\nИндекс максимального значения v3 = " + v3.MaxIndex());
+
+
